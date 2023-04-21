f0e2037adb21a3d7b720d2d288033939403aa78e
TypeHintsFun <mikhailsmnv22@gmail.com>
2023-04-21 22:31:52 +0800

    Сделал асинхронный менеджер контекста

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ import httpx
 
 
 async def main():
-    web_client = httpx.AsyncClient(
-        base_url='http://api.datsart.dats.team/',
-        headers={'Authorization': 'Bearer 643d26392556f643d263925571'}
-    )
+    async with httpx.AsyncClient(
+            base_url='http://api.datsart.dats.team/',
+            headers={'Authorization': 'Bearer 643d26392556f643d263925571'}
+    ) as web_client:
+        await web_client.post('/art/stage/next', headers={})
 
 
 asyncio.run(main())
