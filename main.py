e6f425d9e3e6f1502472b7c9c13592e9e4ab5cec
name <mrfuckingbox@mail.ru>
2023-04-22 01:19:03 +0800

    мультипарт ебля

--- main.py
+++ main.py
@@ -21,12 +21,12 @@ async def main():
             base_url='http://api.datsart.dats.team/',
             headers={'Authorization': 'Bearer 643d26392556f643d263925571'}
     ) as web_client:
+        data = {'imageId': '2'}
         resp: httpx.Response = await web_client.post(
-            '/art/stage/next-start',
-            headers={'Content-Type': 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW;',
-                     'Content-Disposition': 'form-data;'
-                                            'imageId="2"'}
-            # content=data
+            '/art/stage/info',
+            headers={'Content-Type': 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW'}
+            # headers={'Content-Disposition': 'form-data; imageId="2"'}
+            # data=data
         )
 
         print(resp.status_code)
