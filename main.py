f3fefaea337708d644eb6aad01c6d216551dd13d
TypeHintsFun <mikhailsmnv22@gmail.com>
2023-04-21 22:37:50 +0800

    Нужно меньше асинхронных контекстных менеджеров

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ async def main():
             base_url='http://api.datsart.dats.team/',
             headers={'Authorization': 'Bearer 643d26392556f643d263925571'}
     ) as web_client:
-        async with web_client.post(
+        resp: httpx.Response = await web_client.post(
             '/art/stage/next',
             headers={'Content-Type': 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW'}
-        ) as resp:  # type: httpx.Response
-            resp
+        )
+        print(resp.json())
 
 
 asyncio.run(main())
