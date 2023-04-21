9a6803de413de78fb61e74b75a4c0605d158cd7e
TypeHintsFun <mikhailsmnv22@gmail.com>
2023-04-21 22:24:07 +0800

    База мэина

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-# This is a sample Python script.
+import asyncio
+
+import httpx
+
+
+async def main():
+    web_client = httpx.AsyncClient(
+        base_url='http://api.datsart.dats.team/',
+        headers={'Authorization': 'Bearer 643d26392556f643d263925571'}
+    )
+
+
+asyncio.run(main())
