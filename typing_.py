4c05a723b42d61fcbc1aa345d19037d82ef4361f
TypeHintsFun <mikhailsmnv22@gmail.com>
2023-04-21 23:36:23 +0800

    Апдейт типизации

--- typing_.py
+++ typing_.py
@@ -5,11 +5,11 @@ class ResponseInfo(FriendlyDict):
     m: int
     t: float
     c: int
-    tick: int
-    ns: int
+    tick: int | None = None
+    ns: int | None = None
 
 
-class StageInfo(FriendlyDict):
+class Stage(FriendlyDict):
     id: int
     num: int
     name: str
@@ -24,12 +24,52 @@ class Image(FriendlyDict):
     height: int
 
 
-class ResponseField(FriendlyDict):
-    stage: StageInfo
+class GetNextStageResponseField(FriendlyDict):
+    stage: Stage
     images: list[Image]
+
+
+class Queue(FriendlyDict):
+    id: int
+
+
+class StartNextStageResponseField(FriendlyDict):
+    queue: Queue
+
+
+class ShortImage(FriendlyDict):
+    id: int
+    url: str
+    grade: int
+
+
+class Canvas(FriendlyDict):
+    url: str
+
+
+class Stats(FriendlyDict):
+    userStageId: int
+    userId: int
+    lastLogId: int
+    commandCount: int
+    grade: int
+    pixels: int
+    pixelsMisses: int
+    shoots: int
+    shootsMisses: int
+    shootsMissesPartially: int
+
+
+class GetActualInfoStageResponseField(FriendlyDict):
+    stage: Stage
+    images: list[Image]
+    selectedImage: ShortImage
+    canvas: Canvas
+    stats: Stats
 
 
 class ServerResponse(FriendlyDict):
     success: bool
     status: int
     info: ResponseInfo
+    response: GetNextStageResponseField | StartNextStageResponseField | GetActualInfoStageResponseField
