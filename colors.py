a0d8761e9edc9882f2535e9c5bbe53587b5253c1
TypeHintsFun <mikhailsmnv22@gmail.com>
2023-04-22 09:31:20 +0800

    Изменил нейминг и немножко оптимизировал

--- colors.py
+++ colors.py
@@ -12,18 +12,27 @@ def distance(color1, color2):
 
 
 def closest_color(json_list):
-    red = tuple([194, 39, 45])
-    black = tuple([36, 36, 36])
+    red = (194, 39, 45)
+    black = (36, 36, 36)
 
     lowest_distance_red = 5000
     lowest_distance_black = 5000
-    lowest_black_color = ""  # запоминаем цвета
-    lowest_red_color = ""
+
+    closest_black_color = ""  # запоминаем цвета
+    closest_red_color = ""
+
     for color in json_list:  # 24bit color
         rgb = encode_color(int(color))  # получили ргб
+
         red_difference = distance(red, rgb)
         black_difference = distance(black, rgb)
-        if red_difference < lowest_distance_red: lowest_distance_red = red_difference; lowest_red_color = color
-        if black_difference < lowest_distance_black: lowest_distance_black = black_difference; lowest_black_color = color
 
-    return (lowest_red_color, lowest_black_color)
+        if red_difference < lowest_distance_red:
+            lowest_distance_red = red_difference
+            closest_red_color = color
+
+        if black_difference < lowest_distance_black:
+            lowest_distance_black = black_difference
+            closest_black_color = color
+
+    return closest_red_color, closest_black_color
